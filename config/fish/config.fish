1f8f7d5a14b31578c182d79a21c15d60d8686ef0
Michael Barrett <mike182uk@gmail.com>
2026-01-30 10:20:01 +0000

    fix local bin paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -78,8 +78,8 @@ set --global LESS '--mouse'
 
 # Init paths
 
-fish_add_path $HOME/.local/bin
-fish_add_path $HOME/.bun/bin
+fish_add_path "$HOME/.local/bin"
+fish_add_path "$HOME/.bun/bin"
 
 # Init homebrew
 
